a41a026b211b9ad045efe60f282e3718957152df
Ricardovp21 <ricardo.villarreal.p@hotmail.com>
2025-04-06 20:43:49 -0600

    nada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,11 +66,14 @@ dependencies {
     implementation ("com.squareup.picasso:picasso:2.71828")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.swiperefreshlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
