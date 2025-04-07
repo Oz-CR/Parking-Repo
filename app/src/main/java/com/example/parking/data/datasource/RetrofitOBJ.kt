0934b914cb9d938c1f85d61420aa0558931e3bf7
Ricardovp21 <ricardo.villarreal.p@hotmail.com>
2025-04-06 20:45:38 -0600

    swipe
    swiper

--- app/src/main/java/com/example/parking/data/datasource/RetrofitOBJ.kt
+++ app/src/main/java/com/example/parking/data/datasource/RetrofitOBJ.kt
@@ -9,10 +9,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitOBJ {
     companion object {
-        private const val BASE_URL_AUTH = "https://7811-177-244-54-50.ngrok-free.app/api/auth/"
-        private const val BASE_URL_USERS = "https://7811-177-244-54-50.ngrok-free.app/api/users/"
-        private const val BASE_URL_PARKING = "https://7811-177-244-54-50.ngrok-free.app/api/parking/"
-        private const val BASE_URL_SENSORS = "https://7811-177-244-54-50.ngrok-free.app/api/sensors/"
+        private const val BASE_URL_AUTH = "https://280e-189-244-72-23.ngrok-free.app/api/auth/"
+        private const val BASE_URL_USERS = "https://280e-189-244-72-23.ngrok-free.app/api/users/"
+        private const val BASE_URL_PARKING = "https://280e-189-244-72-23.ngrok-free.app/api/parking/"
+        private const val BASE_URL_SENSORS = "https://280e-189-244-72-23.ngrok-free.app/api/sensors/"
 
         fun getRetrofitAuth(context: Context): Retrofit {
             val token = context.getSharedPreferences("userToken", MODE_PRIVATE)
